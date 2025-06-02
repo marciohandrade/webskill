51499e83efc63e8235760debc1725e476c320338
Marcio <marcio.andrade@salgadofilho.com.br>
2025-06-02 09:18:15 -0300

    ajustes no banner e menus

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 /* Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 }); */
+
+Route::get('/', function () {
+    return view('index');
+});
 
